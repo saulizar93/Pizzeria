afcfb8f89379bf4157ee23650bfce1c9243de504
Saul Ojeda <saulojeda@Sauls-MacBook-Pro.local>
2020-12-15 12:50:31 -0800

    Token on console

---
+++ src/components/loginPage/LoginPage.js
@@ -0,0 +1,156 @@
+import React, {useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+//import FormControlLabel from '@material-ui/core/FormControlLabel';
+//import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+//import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://material-ui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignIn() {
+
+    const [userCredentials, setUserCredentials] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(e){
+        setUserCredentials({
+            ...userCredentials,
+            [e.target.name]:e.target.value
+        })
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(userCredentials);
+
+        fetch('http://localhost:8080/auth',{
+            method: 'POST',
+            body: JSON.stringify({
+                email:userCredentials.email,
+                password:userCredentials.password
+            }),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }).then( (res)=>res.json())
+        .then( (res)=>{
+            console.log(res)
+        })
+        .catch( (err)=>console.log(err))
+    }
+
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form onSubmit={handleSubmit} onChange={handleChange} className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        style={{ backgroundColor: 'red' }}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        style={{ backgroundColor: 'red' }}
+                    />
+                    {/* <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    /> */}
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className={classes.submit}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        {/* <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid> */}
+                        {/* <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid> */}
+                    </Grid>
+                </form>
+            </div>
+            {/* <Box mt={8}> */}
+            {/* <Copyright /> */}
+            {/* </Box> */}
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </Container>
+    );
+}
